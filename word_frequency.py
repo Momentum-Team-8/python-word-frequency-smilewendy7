809fbf680a07be4025c9e3079031ac0f79ea8e38
smilewendy7 <smilewendy7@gmail.com>
2021-06-08 18:36:13 -0400

    able to count words, but need to get rid of puntuations

--- word_frequency.py
+++ word_frequency.py
@@ -1,3 +1,5 @@
+
+
 STOP_WORDS = [
     'a', 'an', 'and', 'are', 'as', 'at', 'be', 'by', 'for', 'from', 'has', 'he',
     'i', 'in', 'is', 'it', 'its', 'of', 'on', 'that', 'the', 'to', 'were',
@@ -7,7 +9,36 @@ STOP_WORDS = [
 
 def print_word_freq(file):
     """Read in `file` and print out the frequency of words in that file."""
-    pass
+    
+
+    file = open ("praise_song_for_the_day.txt") 
+
+    text = file.read().split(" ")
+
+    import string
+    text_no_punctuation= [ word.replace(string.punctuation, "") for word in text]
+
+    # line.translate(line.maketrans("", "", string.punctuation))
+
+    print(text_no_punctuation)
+
+    # text.replace (STOP_WORDS, "")
+
+    len(text)
+    text[:20]
+    print(text.count('we'))
+    words = set(text)
+
+    'hello' in words
+    # false
+
+    # for loop count words 
+    for word in text_no_punctuation:
+        # print(word)
+        print(f" {word}, {text.count(word)} ")
+    return text_no_punctuation.count(word)
+        
+
 
 
 if __name__ == "__main__":
