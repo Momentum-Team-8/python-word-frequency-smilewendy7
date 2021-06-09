6809f71b432b7f92514f7efc3a75397a049ad85e
smilewendy7 <smilewendy7@gmail.com>
2021-06-09 16:51:45 -0400

    this version looks good, issue of countting is fixed

--- word_frequency.py
+++ word_frequency.py
@@ -3,7 +3,7 @@
 STOP_WORDS = [
     'a', 'an', 'and', 'are', 'as', 'at', 'be', 'by', 'for', 'from', 'has', 'he',
     'i', 'in', 'is', 'it', 'its', 'of', 'on', 'that', 'the', 'to', 'were',
-    'will', 'with'
+    'will', 'with', ''
 ]
 
 
@@ -12,43 +12,40 @@ def print_word_freq(file):
 
     # with open(file) as the_hill_we_climb:
     #     text = the_hill_we_climb.read().lower()
-    #     str = repr(text[0:-1]) 
+    #     str = repr(text[0:-1])
     #     print(str)
 
     #     print(str.strip(".,'"))
     import re
-    
-    file = open ("praise_song_for_the_day.txt") 
+    import string
+    file = open("praise_song_for_the_day.txt")
     text = file.read().lower()
-    
-    # success!!! ***************************** 
-    text_split= re.split("[' '\n’]", text)
+
+    # success!!! *****************************
+    text_split = re.split("[' '\n’]", text)
     # text = file.read().split(" ")
     # print(text_split)
 
-    ### remove STOP_WORDS in file:
+    # remove STOP_WORDS in file:
     # text_no_stop= [ text.replace(for i in STOP_WORDS, "")]
     # print(text_no_stop)
-
 
     # import string
 
     # remove puncuation
-    text_no_punctuation= [ word.strip(",.'") for word in text_split]
+    text_no_punctuation = sorted([word.strip(string.punctuation) for word in text_split])
     # print(text_no_punctuation)
     # [ word.strip([string.punctuation]) for word in text]
     # line.translate(line.maketrans("", "", string.punctuation))
 
     # remove STOP_WORDS from text_no_punctuation
-
     #!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-    # for element in STOP_WORDS:
-    #     if element in text_no_punctuation:
-    #         text_no_punctuation.remove(element)
-        
-    # print(f"stop {STOP_WORDS}")
-    # print(f"text {text_no_punctuation}")
+    for element in STOP_WORDS:
+        while element in text_no_punctuation:
+            text_no_punctuation.remove(element)
     
+    print(text_no_punctuation)
+
     # print(text_no_punctuation.remove(element))
     #!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 
@@ -61,14 +58,18 @@ def print_word_freq(file):
     # 'hello' in words
     # false
 
-    # for loop count words 
+    # for loop count words
+    result ={}
     for word in text_no_punctuation:
         # print(word)
-        print(f" {word}, {text.count(word)} ")
-    return text_no_punctuation.count(word)
-        
+        result[f"{word}"]= text_no_punctuation.count(word)
+    print(result)
+    #     print(f" {word}, {text_no_punctuation.count(word)} ")
+    # return text_no_punctuation.count(word)
 
-
+    for count in result:
+        star = "*" * result[count]
+        print(f"{count}  |  {result[count]} {star}"  )
 
 if __name__ == "__main__":
     import argparse
