ecf8595ece7ab8d83cd96756c49fe63bcdaea3e4
smilewendy7 <smilewendy7@gmail.com>
2021-06-08 23:42:08 -0400

    split issue fixed, need to remove STOP_WORDS

--- word_frequency.py
+++ word_frequency.py
@@ -9,27 +9,56 @@ STOP_WORDS = [
 
 def print_word_freq(file):
     """Read in `file` and print out the frequency of words in that file."""
+
+    # with open(file) as the_hill_we_climb:
+    #     text = the_hill_we_climb.read().lower()
+    #     str = repr(text[0:-1]) 
+    #     print(str)
+
+    #     print(str.strip(".,'"))
+    import re
     
-
     file = open ("praise_song_for_the_day.txt") 
+    text = file.read().lower()
+    
+    # success!!! ***************************** 
+    text_split= re.split("[' '\n’]", text)
+    # text = file.read().split(" ")
+    # print(text_split)
 
-    text = file.read().split(" ")
+    ### remove STOP_WORDS in file:
+    # text_no_stop= [ text.replace(for i in STOP_WORDS, "")]
+    # print(text_no_stop)
 
-    import string
-    text_no_punctuation= [ word.replace(string.punctuation, "") for word in text]
 
+    # import string
+
+    # remove puncuation
+    text_no_punctuation= [ word.strip(",.'") for word in text_split]
+    # print(text_no_punctuation)
+    # [ word.strip([string.punctuation]) for word in text]
     # line.translate(line.maketrans("", "", string.punctuation))
 
-    print(text_no_punctuation)
+    # remove STOP_WORDS from text_no_punctuation
 
-    # text.replace (STOP_WORDS, "")
+    #!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+    # for element in STOP_WORDS:
+    #     if element in text_no_punctuation:
+    #         text_no_punctuation.remove(element)
+        
+    # print(f"stop {STOP_WORDS}")
+    # print(f"text {text_no_punctuation}")
+    
+    # print(text_no_punctuation.remove(element))
+    #!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 
-    len(text)
-    text[:20]
-    print(text.count('we'))
-    words = set(text)
+    # print(text)
+    # len(text)
+    # text[:20]
+    # print(text.count('we'))
+    # words = set(text)
 
-    'hello' in words
+    # 'hello' in words
     # false
 
     # for loop count words 
